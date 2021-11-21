a8e6dc83fb5e44489dfa4e00ce20f65a00bb64e1
kenbom <tsutomu.kimura@nifty.ne.jp>
2021-11-21 08:24:12 +0000

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from "react"
+import styled from "styled-components"
+import Logo from "../assets/img/logo.svg"
+
+export default function Home() {
+  return (
+  <div>
+  <img src="../assets/img/logo.svg" alt="logo"></img>
+  <Div>Hello world!</Div>
+  </div>
+  )
+}
+
+const Div =styled.div`
+color:white;
+font-size: 2rem;
+background-color: gray;
+`
