693955126b0338fd75fdf51b15d670d7ce227d3e
kenbom <tsutomu.kimura@nifty.ne.jp>
2021-11-27 04:40:16 +0000

    add pictures

--- src/pages/{ContentfulSleepies.title}.js
+++ src/pages/{ContentfulSleepies.title}.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { graphql } from 'gatsby'
+import { graphql, Link } from 'gatsby'
 import slugify from 'slugify'
+import {GatsbyImage, getImage} from "gatsby-plugin-image"
 
 const ContentfulSleepies = ({data}) => {
     const titleTag = data.contentfulSleepies.title
@@ -9,6 +10,8 @@ const ContentfulSleepies = ({data}) => {
         <div>
            <h1>{titleTag}</h1>
            <h1>{sluggedTitle}</h1>
+           <Link to={`/${sluggedTitle}`}>Ungry Link</Link>
+           <GatsbyImage image={data.contentfulSleepies.picture.gatsbyImageData} alt="ungry"></GatsbyImage>
         </div>
     )
 }
@@ -17,6 +20,12 @@ export const query = graphql`
   query getSinglePage($title:String){
     contentfulSleepies(title: {eq: $title}) {
       title  
+      desc {
+        raw
+      }
+      picture {
+        gatsbyImageData(placeholder: BLURRED)
+      }
     }
   }
 `
